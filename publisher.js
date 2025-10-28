6e8fc0a0aa3e6f487f1b41d8ba1473120046952b
Yusuf Baskeser <yusufbaskeser2005@gmail.com>
2025-10-28 13:42:08 +0300

    changes

--- publisher.js
+++ publisher.js
@@ -1,4 +1,11 @@
 const amqp = require('amqplib/callback_api');
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 amqp.connect('amqp://localhost:5672', (err, connection) => {
   if (err) {
@@ -8,8 +15,14 @@ connection.createChannel((err, channel) => {
     if (err) {
       throw err;
     }
+
+    // 3 tane kuyruk tanımladım
     let queueName = 'hello_queue';
     let queueName2 = "hello_queue2";
+    let userQueue= "UserQueue";
+
+
+  //" Mesajlarımı tanımlıyorum
 
     let msg = "Hello World!";
     let messages = [
@@ -19,18 +32,36 @@ connection.createChannel((err, channel) => {
       ":)"
     ];
 
-      channel.assertQueue(queueName, { durable: true });
+    // Kuyrukları tanımlıyorum
+    channel.assertQueue(queueName, { durable: true });
     channel.assertQueue(queueName2, { durable: true });
+    channel.assertQueue(userQueue, { durable: true });
+    
 
+// Burda kullanıcıdan input alıp userQueue'ya mesaj gönderiyorum
+    rl.on('line', (input) => {
+     if (input.toLowerCase() === 'exit') {
+        console.log("Disconnecting...");
+        connection.close();
+        rl.close();
+        return;
+      }
+      channel.sendToQueue(userQueue, Buffer.from(input));
+      console.log("Message to userQueue: " + input);
+    });
+
+// Daha önce tanımladığım mesajları ikinci kuyruğa gönderiyorum
     messages.forEach(msg => {
     
       channel.sendToQueue(queueName2, Buffer.from(msg));
       console.log("Message to queue2: " + msg);
     });
 
- 
+ // aynı şekilde ilk kuyruğa da mesaj gönderiyorum
     channel.sendToQueue(queueName, Buffer.from(msg));
     console.log("Message to queue1: " + msg);
+    
+    // Bağlantıyı 1 saniye sonra kapatıyorum
     setTimeout(() => {
         connection.close();
     },1000);
--- subscriber.js
+++ subscriber.js
@@ -4,27 +4,39 @@ amqp.connect('amqp://localhost:5672', (err, connection) => {
   if (err) {
     throw err;
   }
+  
 connection.createChannel((err, channel) => {
     if (err) {
       throw err;
     }
     let queueName = 'hello_queue';
     let queueName2 = "hello_queue2";
+    let userQueue= "UserQueue";
+ // Kuyrukları tanımlıyorum eğer oluşturulmamışsa oluşturulsun diye
+    channel.assertQueue(userQueue, { durable: true });
 
     channel.assertQueue(queueName2, {
         durable: true
     });
+    channel.assertQueue(queueName, {
+        durable: true
+    });
+
+
+    // Kullanıcıdan gelen mesajları dinliyorum
+    channel.consume(userQueue, (msg) => {
+        console.log("Received from userQueue:" + msg.content.toString());
+    });
+
     channel.consume(queueName2, (msg) => {
         console.log("Received from queue2:" + msg.content.toString());
         channel.ack(msg);
     });
-    channel.assertQueue(queueName, {
-        durable: true
-    });
-  channel.consume(queueName, (msg) => {
+  
+    channel.consume(queueName, (msg) => {
       console.log("Received from queue1:" + msg.content.toString());
   });
-
+ // Bağlantıyı 1 saniye sonra kapatıyorum
   setTimeout(() => {
         connection.close();
     },1000);
